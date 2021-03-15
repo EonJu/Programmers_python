3a9d18bccae1e2e4c6f64cf34cd0545148974469
EonJu <may701@naver.com>
2021-03-16 00:27:56 +0900

    first commit

---
+++ kakao_crain(64061).py
@@ -0,0 +1,24 @@
+
+def solution(board, moves):
+    answer = 0
+    arr=[]
+    print("Test")
+    for i in moves:
+        for j in range(len(board)):
+            if(board[j][i-1] == 0):
+                pass
+            else:
+                arr.append(board[j][i-1])
+                board[j][i-1] = 0
+                break
+        if(len(arr)>=2 and arr[-1]==arr[-2]):
+            answer+=2
+            arr.pop(-1)
+            arr.pop(-1)
+                
+    print(answer)
+    return answer
+
+board=[[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves =[1,5,3,5,1,2,1,4]
+solution(board,moves)
